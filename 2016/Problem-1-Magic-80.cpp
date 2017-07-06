fc70e599f368125a71ea8d5d5fcb8d2a9af2bb76
x4nter <harmudharh@gmail.com>
2017-07-05 23:14:40 -0500

    Update Problem-1-Magic-80.cpp

--- 2016/Problem-1-Magic-80.cpp
+++ 2016/Problem-1-Magic-80.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 int main(){
     for(int i = 18; i <= 81; i++){
         int age = i, worked = 0;
@@ -9,6 +7,6 @@ int main(){
             age++;
             worked++;
         }
-        cout << "If you are " << i << " you can retire at age " << age << endl;
+        std::cout << "If you are " << i << " you can retire at age " << age << std::endl;
     }
 }
