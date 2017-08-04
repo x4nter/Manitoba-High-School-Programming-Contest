b8b626c3e4da94787d04647f480a8c3be5f63ab7
x4nter <harmudharh@gmail.com>
2017-08-04 13:29:08 -0500

    Update Problem-1-PIN-possibilities.cpp

--- 2015/Problem-1-PIN-possibilities.cpp
+++ 2015/Problem-1-PIN-possibilities.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
 	for (int i = 1; i <= 10; i++)
-		cout << (long)pow(8, i) << endl;
+		std::cout << (long)pow(8, i) << std::endl;
 	return 0;
 }
